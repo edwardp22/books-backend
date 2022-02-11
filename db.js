8a54fc03b16213cb36024db4cbdd9f4f927efc26
edwardp22 <58954908+edwardp22@users.noreply.github.com>
2022-02-11 05:10:39 -0800

    Backend implementing express

---
+++ db.js
@@ -0,0 +1,16 @@
+module.exports = [
+    {
+        id: 1,
+        name: "MSI",
+        author: "Bidgoli, Hossein",
+        publishingYear: 2020,
+        ISBN: "9780357418697",
+    },
+    {
+        id: 2,
+        name: "C# Programming: From Problem Analysis to Program Design",
+        author: "Doyle, Barbara",
+        publishingYear: 2015,
+        ISBN: 9781285856872,
+    }
+]
---
+++ routes/books.js
@@ -0,0 +1,69 @@
+var express = require("express");
+var router = express.Router();
+
+var books = require("../db.js");
+
+/* GET books listing. */
+router.get("/", (req, res, next) => {
+  try {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json");
+    res.json(books);
+  } catch (error) {
+    res.statusCode = 400;
+    res.send(error);
+  }
+});
+
+/* Create book. */
+router.post("/", (req, res, next) => {
+  try {
+    if (Array.isArray(req.body)) books.push(...req.body);
+    else if (Object.keys(req.body).length !== 0) books.push(req.body);
+
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json");
+    res.json(books);
+  } catch (error) {
+    res.statusCode = 400;
+    res.send(error);
+  }
+});
+
+/* Put book. */
+router.put("/:id", (req, res, next) => {
+  try {
+    let rowIndex = books.findIndex((book) => book.id === +req.params.id);
+
+    if (rowIndex !== -1 && Object.keys(req.body).length !== 0) {
+      books.splice(rowIndex, 1, req.body);
+    }
+
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json");
+    res.json(books);
+  } catch (error) {
+    res.statusCode = 400;
+    res.send(error);
+  }
+});
+
+/* Delete book. */
+router.delete("/:id", (req, res, next) => {
+  try {
+    let rowIndex = books.findIndex((book) => book.id === +req.params.id);
+
+    if (rowIndex !== -1) {
+      books.splice(rowIndex, 1);
+    }
+
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json");
+    res.json(books);
+  } catch (error) {
+    res.statusCode = 400;
+    res.send(error);
+  }
+});
+
+module.exports = router;
