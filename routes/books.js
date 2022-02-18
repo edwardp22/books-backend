5a35a3b9a96856065041c5e7edd298b2459f496c
Edward Peña <58954908+edwardp22@users.noreply.github.com>
2022-02-18 04:24:44 -0800

    Using data structures
    faster searching of records

--- routes/books.js
+++ routes/books.js
@@ -2,6 +2,8 @@ var express = require("express");
 var router = express.Router();
 
 var books = require("../db.js");
+let booksIndex = [];
+books.forEach((book, index) => booksIndex[book.id] = index);
 
 /* GET books listing. */
 router.get("/", (req, res, next) => {
@@ -18,12 +20,12 @@ router.get("/", (req, res, next) => {
 /* Create book. */
 router.post("/", (req, res, next) => {
   try {
-    if (Array.isArray(req.body)) books.push(...req.body);
-    else if (Object.keys(req.body).length !== 0) {
+    if (Object.keys(req.body).length !== 0) {
       req.body.id = books.length
         ? Math.max(...books.map((book) => book.id)) + 1
         : 1;
       books.push(req.body);
+      booksIndex[req.body.id] = books.length - 1;
     }
 
     res.statusCode = 200;
@@ -38,7 +40,7 @@ router.post("/", (req, res, next) => {
 /* Put book. */
 router.put("/:id", (req, res, next) => {
   try {
-    let rowIndex = books.findIndex((book) => book.id === +req.params.id);
+    const rowIndex = booksIndex[+req.params.id];
 
     if (rowIndex !== -1 && Object.keys(req.body).length !== 0) {
       books.splice(rowIndex, 1, req.body);
@@ -56,10 +58,11 @@ router.put("/:id", (req, res, next) => {
 /* Delete book. */
 router.delete("/:id", (req, res, next) => {
   try {
-    let rowIndex = books.findIndex((book) => book.id === +req.params.id);
+    const rowIndex = booksIndex[+req.params.id];
 
     if (rowIndex !== -1) {
       books.splice(rowIndex, 1);
+      booksIndex.splice(+req.params.id, 1)
     }
 
     res.statusCode = 200;
